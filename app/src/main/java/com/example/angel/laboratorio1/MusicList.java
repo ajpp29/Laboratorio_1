583cc92fd443a4166c9dec2b451d00a0d5bc6a7d
Angel Jimenez <angeljpp29@gmail.com>
2018-09-04 01:23:57 -0600

    Creación de las canciones de la lista de reproducción

--- app/src/main/java/com/example/angel/laboratorio1/MusicList.java
+++ app/src/main/java/com/example/angel/laboratorio1/MusicList.java
@@ -1,6 +1,21 @@
 package com.example.angel.laboratorio1;
 
 
+import android.support.v7.util.SortedList;
+
 public class MusicList {
+
+    public SortedList<Cancion> ListaReproduccion;
+
+    Cancion cancion1=new Cancion("animals",4,"Martin Garrix");
+    Cancion cancion2=new Cancion("all falls down",4,"Alan Walker,Noah Cyrus");
+    Cancion cancion3=new Cancion("born to be yours",4,"Kygo,Imagine Dragons");
+    Cancion cancion4=new Cancion("closer",4,"The Chainsmokers");
+    Cancion cancion5=new Cancion("feels",4,"Calvin Harris,Pharrell Williams");
+    Cancion cancion6=new Cancion("go to sleep",4,"John DE Sohn");
+    Cancion cancion7=new Cancion("in my mind",4,"Dynoro,Gigi D'Agostino");
+    Cancion cancion8=new Cancion("lie to me",4,"Steve Aoki,Ina Wroldsen");
+    Cancion cancion9=new Cancion("ocean",4,"Martin Garrix,Khalid");
+    Cancion cancion10=new Cancion("only you",4,"Cheat Codes,Little Mix");
 
 }
