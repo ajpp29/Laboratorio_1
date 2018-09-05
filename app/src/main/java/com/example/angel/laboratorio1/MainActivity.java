2b303a151ac81673db93c8465cda8c3c0df1e716
Angel Jimenez <angeljpp29@gmail.com>
2018-09-04 23:53:29 -0600

    Modificación del Main y la clase MusicList

--- app/src/main/java/com/example/angel/laboratorio1/MainActivity.java
+++ app/src/main/java/com/example/angel/laboratorio1/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.angel.laboratorio1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    Button btnReproductor;
+    MusicList lista;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnReproductor=(Button) findViewById(R.id.btnReproduccion);
+        lista=new MusicList();
+
+        btnReproductor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                lista.IniciarReproductor();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/angel/laboratorio1/MusicList.java
+++ app/src/main/java/com/example/angel/laboratorio1/MusicList.java
@@ -9,20 +9,18 @@ public class MusicList {
 
     public LinkedList<Cancion> ListaReproduccion;
 
-    Cancion cancion1;
-    Cancion cancion2;
-    Cancion cancion3;
-    Cancion cancion4;
-    Cancion cancion5;
-    Cancion cancion6;
-    Cancion cancion7;
-    Cancion cancion8;
-    Cancion cancion9;
-    Cancion cancion10;
+    private Cancion cancion1;
+    private Cancion cancion2;
+    private Cancion cancion3;
+    private Cancion cancion4;
+    private Cancion cancion5;
+    private Cancion cancion6;
+    private Cancion cancion7;
+    private Cancion cancion8;
+    private Cancion cancion9;
+    private Cancion cancion10;
 
     public MusicList(){
-        LinkedList<Cancion> ListaReproduccion= new LinkedList<Cancion>();
-
         cancion1=new Cancion("animals",5.04,"Martin Garrix");
         cancion2=new Cancion("all falls down",3.19,"Alan Walker,Noah Cyrus");
         cancion3=new Cancion("born to be yours",3.13,"Kygo,Imagine Dragons");
@@ -33,10 +31,11 @@ public class MusicList {
         cancion8=new Cancion("lie to me",2.59,"Steve Aoki,Ina Wroldsen");
         cancion9=new Cancion("ocean",3.36,"Martin Garrix,Khalid");
         cancion10=new Cancion("only you",3.09,"Cheat Codes,Little Mix");
-        IniciarReproductor();
+        //IniciarReproductor();
     }
 
     public void IniciarReproductor(){
+        LinkedList<Cancion> ListaReproduccion= new LinkedList<Cancion>();
         ListaReproduccion.add(cancion1);
         ListaReproduccion.add(cancion2);
         ListaReproduccion.add(cancion3);
