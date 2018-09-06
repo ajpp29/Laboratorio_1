0b2058d1980ae02619b50fee488612ab2b5eb534
Angel Jimenez <angeljpp29@gmail.com>
2018-09-05 18:40:05 -0600

    Modificación del menu principal

--- app/src/main/java/com/example/angel/laboratorio1/MainActivity.java
+++ app/src/main/java/com/example/angel/laboratorio1/MainActivity.java
@@ -28,8 +28,7 @@ public class MainActivity extends AppCompatActivity {
         btnReproductor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
+                
                 lvReproductor.setAdapter(adaptador);
             }
         });
